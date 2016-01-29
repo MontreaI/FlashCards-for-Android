29fff851d68bf37d0033811926bb33496ce0ffca
MontreaI <raymondchan1995@hotmail.com>
2016-01-28 20:09:48 -0800

    Addition of source code

---
+++ FlashCards/app/src/main/java/com/example/windowsftw/flashcards/FlashCard.java
@@ -0,0 +1,18 @@
+package com.example.windowsftw.flashcards;
+
+
+/**
+ * Created by windows.ft.w on 9/6/2015.
+ */
+public class FlashCard {
+    public FlashCard(){
+        name = "Untitled";
+        numberCards = 0;
+        subject = Subjects.COMPUTINGSCIENCE;
+    }
+
+    private String name;
+    private int numberCards;
+    private Subjects subject;
+
+}
---
+++ FlashCards/app/src/main/java/com/example/windowsftw/flashcards/Subjects.java
@@ -0,0 +1,8 @@
+package com.example.windowsftw.flashcards;
+
+/**
+ * Created by windows.ft.w on 9/6/2015.
+ */
+public enum Subjects {
+    COMPUTINGSCIENCE
+}
